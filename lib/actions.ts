0c80b1dfd10a1c0594894cbbe346cb395424d82e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-17 16:28:05 +0700

    creating updateContact & deleteContact server action

--- lib/actions.ts
+++ lib/actions.ts
@@ -34,6 +34,59 @@ export const saveContact = async (prevState: any, formData: FormData) => {
     };
   }
 
-	revalidatePath("/contacts");
-	redirect("/contacts");
+  revalidatePath("/contacts");
+  redirect("/contacts");
+};
+
+export const updateContact = async (
+  id: string,
+  prevState: any,
+  formData: FormData,
+) => {
+  const validatedFields = ContactSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
+
+  if (!validatedFields.success) {
+    return {
+      Error: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  try {
+    await prisma.contact.update({
+      data: {
+        name: validatedFields.data.name,
+        phone: validatedFields.data.phone,
+      },
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Failed to update contact",
+    };
+  }
+
+  revalidatePath("/contacts");
+  redirect("/contacts");
+};
+
+export const deleteContact = async (
+  id: string,
+) => {
+  try {
+    await prisma.contact.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Failed to delete contact",
+    };
+  }
+
+  revalidatePath("/contacts");
 };

991129e57e7f9e78129720b7c4e13d49624b595e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-17 16:29:17 +0700

    creating getContact & getContactById functionalities for fetching data in data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,19 @@
 import { prisma } from "@/lib/prisma";
 
 export const getContact = async () => {
-	try {
-		const contact = await prisma.contact.findMany();
-		return contact
-	} catch (error) {
-		throw new Error("Failed to fetch contact data")
-	}
+  try {
+    const contact = await prisma.contact.findMany();
+    return contact;
+  } catch (error) {
+    throw new Error("Failed to fetch contact data");
+  }
+};
+
+export const getContactById = async (id: string) => {
+  try {
+    const contact = await prisma.contact.findUnique({ where: { id } });
+    return contact;
+  } catch (error) {
+    throw new Error("Failed to fetch contact data");
+  }
 };

5096d4e43def5e04026fabef20378d4ae7370d00
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-17 16:28:20 +0700

    create cn utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,15 @@
-export const formatDate = (dateStr: string) => {
-	const date = new Date(dateStr);
-	const formatter = new Intl.DateTimeFormat("id-ID", {
-		dateStyle: 'medium',
-		timeStyle: 'short',
-	});
-	return formatter.format(date);
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
+
+export const formatDate = (dateStr: string) => {
+  const date = new Date(dateStr);
+  const formatter = new Intl.DateTimeFormat("id-ID", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  });
+  return formatter.format(date);
+};
